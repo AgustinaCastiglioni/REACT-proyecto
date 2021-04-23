fc6ec17f5fbae3ad70eac34b59152bc664a42078
Agustina <Castiglioni>
2021-04-22 23:22:03 -0300

    Punto 3 y 4 terminado (revisar algunas cosa en Trello)
    Punto 3 y 4 terminado (revisar algunas cosa en Trello)

--- mi-segundo-proyecto/src/components/Headers.js
+++ mi-segundo-proyecto/src/components/Headers.js
@@ -2,7 +2,7 @@ import React from 'react';
 function Header() {
     return (
         <header>
-        <h1 class="titulo">Proyecto Integrador <span className="characters">  REACT </span></h1>
+        <h1 className="titulo">Proyecto Integrador <span className="characters">  REACT </span></h1>
       </header>
     );
   }
